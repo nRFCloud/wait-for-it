8313466c7f1d02e99a750b40bb94930a911b02c8
Markus Tacker <Markus.Tacker@NordicSemi.no>
2025-01-09 11:15:06 +0100

    docs: update

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # `@nrfcloud/wait-for-it`
 
 <https://jsr.io/@nrfcloud/wait-for-it>
+
+## Install with NPM
+
+```bash
+npx jsr add --save-dev @nrfcloud/wait-for-it
+```
+
+## Usage
+
+```typescript
+import * as wait_for_it from "@nrfcloud/wait-for-it";
+const tenant = await wait_for_it<Tenant>(() => repo.getByUUID(e.aggregateUUID));
+```
--- waitForIt.ts
+++ waitForIt.ts
@@ -1,4 +1,4 @@
-import { exponential } from "backoff";
+import { exponential } from 'backoff'
 
 /**
  * @module
@@ -22,28 +22,28 @@ import { exponential } from "backoff";
  * ```
  */
 export const waitForIt = <A>(
-  fn: () => Promise<A>,
-  retryNumber: number = 9
+	fn: () => Promise<A>,
+	retryNumber: number = 9,
 ): Promise<A> =>
-  new Promise((resolve, reject) => {
-    const b = exponential({
-      randomisationFactor: 0,
-      initialDelay: 250,
-      maxDelay: 5000,
-    });
-    let lastErr: Error;
-    b.failAfter(retryNumber);
-    b.on("ready", async () => {
-      try {
-        const res = await fn();
-        return resolve(res);
-      } catch (e) {
-        lastErr = e as Error;
-      }
-      b.backoff();
-    });
-    b.on("fail", () => {
-      reject(lastErr);
-    });
-    b.backoff();
-  });
+	new Promise((resolve, reject) => {
+		const b = exponential({
+			randomisationFactor: 0,
+			initialDelay: 250,
+			maxDelay: 5000,
+		})
+		let lastErr: Error
+		b.failAfter(retryNumber)
+		b.on('ready', async () => {
+			try {
+				const res = await fn()
+				return resolve(res)
+			} catch (e) {
+				lastErr = e as Error
+			}
+			b.backoff()
+		})
+		b.on('fail', () => {
+			reject(lastErr)
+		})
+		b.backoff()
+	})
